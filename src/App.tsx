15b8cedcd2526c489e89a8dffb6e414200a0d7d6
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-11-06 11:08:03 +0000

    define theme and basic global styles in styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { theme, GlobalStyle } from './GlobalStyles'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <h1>Schwarz</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/GlobalStyles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const theme = {
+  bgColor: '#000000',
+  textColor: '#FFFFFF',
+  primary: '#ffd900',
+  secondary: '#F5F5F5',
+  borderColor: '#444444',
+  textFont: "'Lato', sans-serif",
+  headFont: "'Playfair Display', serif",
+}
+
+export const GlobalStyle = createGlobalStyle`
+#root {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+}
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: ${(props) => props.theme.textFont};
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    margin: 0;
+    width: 100vw;
+    min-height: 100vh;
+}
+
+h1:hover,
+h2:hover,
+h3:hover,
+h4:hover,
+h5:hover,
+h6:hover,
+p:hover {
+    cursor: default
+}
+`
+
+//   card - background: linear-gradient(to bottom, #333333, #222222, #111111);

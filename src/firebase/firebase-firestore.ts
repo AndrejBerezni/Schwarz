5f33682b9510a22631d6bf3c7c4f5de712dbd60c
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-11-13 12:26:26 +0000

    create function to get new products from firestore and stripe

---
+++ src/firebase/firebase-firestore.ts
@@ -0,0 +1,36 @@
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+} from 'firebase/firestore'
+import { app } from './firebase-config'
+
+const db = getFirestore(app)
+
+//Get new products
+
+export const getNewProducts = async () => {
+  const productsArray: any[] = []
+  const q = query(collection(db, 'products'), where('metadata.new', '==', '1'))
+  const querySnapshot = await getDocs(q)
+
+  for (const doc of querySnapshot.docs) {
+    const prices: any[] = []
+    const priceQuery = collection(db, 'products', doc.id, 'prices')
+    const priceSnapshot = await getDocs(priceQuery)
+
+    priceSnapshot.forEach((item) => {
+      prices.push(item.data())
+    })
+
+    const productData = doc.data()
+    productData.prices = prices
+    productData.docId = doc.id
+
+    productsArray.push(productData)
+  }
+
+  return productsArray
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,20 @@ import { Outlet } from 'react-router'
 import { StyledHome } from './Home.styles'
 import BrowseCategories from '../../components/BrowseCategories'
 import FeaturedCarousel from '../../components/FeaturedCarousel'
+import { useEffect, useState } from 'react'
+import { getNewProducts } from '../../firebase/firebase-firestore'
 
 export default function Home() {
+  const [products, setProducts] = useState<any[]>([])
+
+  useEffect(() => {
+    const setCarouselProducts = async () => {
+      const newProducts = await getNewProducts()
+      setProducts(newProducts)
+    }
+
+    setCarouselProducts()
+  }, [])
   return (
     <StyledHome>
       <div style={{ display: 'flex' }}>
